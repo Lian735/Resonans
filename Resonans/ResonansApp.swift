aa1034f7bb7386e25141f9837818058f2c3b015e
Lian735 <lianschusterr@icloud.com>
2025-09-07 01:17:10 +0200

    Initial commit

---
+++ Resonans/ResonansApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResonansApp.swift
+//  Resonans
+//
+//  Created by Lian on 07.09.25.
+//
+
+import SwiftUI
+
+@main
+struct ResonansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
